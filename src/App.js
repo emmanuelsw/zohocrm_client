06cf4145d0522669092c7512e06142360e39b1dd
EmmanuelSW <edhgt89@hotmail.com>
2017-09-29 18:50:25 -0500

    initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navigation from './Navigation'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Navigation.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return(
+			<nav className="navbar navbar-default navbar-fixed-top">
+				<div className="container">
+
+					<div className="navbar-header">
+						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span className="sr-only">Toggle navigation</span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+						</button>
+						<a className="navbar-brand" href="#">Zoho CRM Client</a>
+					</div>
+
+					<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul className="nav navbar-nav navbar-right">
+							<li><a href="#">API Source Code</a></li>
+							<li><a href="#">Client Source Code</a></li>
+							<li><a href="#">Emmanuel Deossa</a></li>
+						</ul>
+					</div>
+
+				</div>
+			</nav>
+		);
+  }
+}
+
+export default Navigation;
