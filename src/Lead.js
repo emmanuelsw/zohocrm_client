88bc562751246175c9e2582f1c8b72a13b83480a
EmmanuelSW <edhgt89@hotmail.com>
2017-09-30 20:08:26 -0500

    Errors in alert

--- src/Lead.js
+++ src/Lead.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
-import reqwest from 'reqwest';
+import axios from 'axios';
+import { ToastContainer, ToastMessage } from 'react-toastr';
+const ToastMessageFactory = React.createFactory(ToastMessage.animation);
+
+const options = {
+	timeOut: 5000,
+	extendedTimeOut: 1000, 
+	closeButton: true,
+	showAnimation: 'animated fadeInRight', 
+	hideAnimation: 'animated fadeOutRight',
+	escapeHtml: false
+}
 
 class Lead extends Component {
 
@@ -7,19 +18,59 @@ class Lead extends Component {
 		super(props);
 		this.state = {
 			leads: [],
-			lead_id: 0,
-			phone: 0,
-			lead_source: 0
+			lead_id: '',
+			phone: '',
+			lead_source: 0,
+			search: ''
 		};
 	}
 
 	componentDidMount() {
-
+		
 	}
+
+	handleChange = (e) => {
+    let newState = {};
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+	}
+	
+	handleError = (err) => {
+		const errors = err.response.data.errors;
+		
+		if(typeof errors === 'string') {
+			this.container.error(errors, "", options);
+		} else {
+			let errorsResponse = [];
+			for(let key in errors) {
+				errorsResponse.push(<li key={key}>{errors[key]}</li>)
+			}
+			this.container.error(errorsResponse, "", options);
+		}
+
+  }
+
+	addByLeadID = (e) => {
+		e.preventDefault();
+
+		axios.post('http://localhost:3001/api/v1/leads', {
+			lead_id: this.state.lead_id
+		})
+		.then(response => {
+			console.log(response);
+		})
+		.catch(err => this.handleError(err));
+
+  }
 
   render() {
     return(
 			<div className="container top-space">
+				<ToastContainer ref={(input) => {this.container = input;}}
+					toastMessageFactory={ToastMessageFactory}
+					className="toast-top-right"
+					preventDuplicates={false}
+				/>
 				<div className="row">
 
 					<div className="col-md-6 col-xs-12">
@@ -27,13 +78,13 @@ class Lead extends Component {
 							<legend>Add Lead</legend>
 							<hr className="title-hr"/>
 
-							<form action="" method="post">
+							<form method="post" onSubmit={this.addByLeadID}>
 								<div className="form-group">
 									<label className="control-label" htmlFor="lead_id">Add by Lead ID</label>
 									<div className="input-group">
-										<input className="form-control" id="lead_id" type="text" placeholder="2800097000000135253"/>
+										<input className="form-control" onChange={this.handleChange} name="lead_id" type="text"   placeholder="2800097000000135253"/>
 										<span className="input-group-btn">
-											<button className="btn btn-default" type="button">Add!</button>
+											<button type="submit" className="btn btn-default">Add!</button>
 										</span>
 									</div>
 								</div>
@@ -44,7 +95,7 @@ class Lead extends Component {
 							<form action="" method="post">
 								<div className="form-group">
 									<label className="control-label" htmlFor="lead_id">Search by phone number</label>
-									<input className="form-control" id="lead_id" type="text" placeholder="555-555-5555"/>
+									<input onChange={this.handleChange} className="form-control" name="phone" type="text" placeholder="555-555-5555"/>
 								</div>
 							</form>
 
@@ -58,20 +109,20 @@ class Lead extends Component {
 								<hr className="title-hr"/>
 								<div className="form-group">
 									<label className="control-label" htmlFor="lead_id">Search</label>
-									<input className="form-control" id="lead" type="text" placeholder="Person Name, Phone, or Company..."/>
+									<input onChange={this.handleChange} className="form-control" name="search" type="text" placeholder="Person Name, Phone, or Company..."/>
 								</div>
 
 								<hr className="body-hr"/>
 
 								<div className="form-group">
 									<label className="control-label" htmlFor="lead_id">Filter by Lead Source</label>
-									<select value={this.state.value} className="form-control" id="select" onChange={(e)=>{this.setState({value: e.target.value})}}>
+									<select value={this.state.lead_source} className="form-control" name="lead_source" onChange={this.handleChange}>
 										<option disabled value="0">Select an option</option>
-										<option>1</option>
-										<option>2</option>
-										<option>3</option>
-										<option>4</option>
-										<option>5</option>
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+										<option value="5">5</option>
 									</select>
 								</div>
 							</form>
